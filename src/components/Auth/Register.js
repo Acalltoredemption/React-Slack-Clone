907d8940bd950a84646e1bbaa372cda0780fafa8
Acalltoredemption <acalltoredemption@gmail.com>
2021-08-27 18:14:45 -0700

    working on register

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,17 +1,42 @@
 import React from 'react';
 import {Grid, Form, Segment, Button, Header, Message, Icon} from 'semantic-ui-react';
 import {Link} from 'react-router-dom';
+import firebase from '../../firebase';
 
 class Register extends React.Component {
-    handleChange = () => {}
+    state = {
+        username: '',
+        email: '',
+        password: '',
+        passwordConfirmation: ''
+    };
+    handleChange = event => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    };
+
+    handleSubmit = event => {
+        event.preventDefault();
+        firebase
+        .auth()
+        .createUserWithEmailAndPassword(this.state.email, this.state.password)
+        .then(createdUser => {
+            console.log(createdUser);
+        })
+        .catch(err => {
+            console.error(err);
+        })
+    }
     render() {
+
         return (
             <Grid textAlign="center" verticalAlign="middle" className="app">  
                 <Grid.Column style={{maxWidth: 450}}>
                     <Header as="h2" icon color="orange" textAlign="center">
                         <Icon name="puzzle piece" color="orange" />
                     </Header>
-                    <Form size="large">
+                    <Form size="large" onSubmit={this.handleSubmit}>
                         <Segment stacked>
                             <Form.Input fluid name="username" icon="user" iconPosition="left"
                             placeholder="Username" onChange={this.handleChange} type="text" />
@@ -20,9 +45,9 @@ class Register extends React.Component {
                             placeholder="Email Address" onChange={this.handleChange} type="email" />
 
                             <Form.Input fluid name="password" icon="lock" iconPosition="left"
-                            placeholder="Password" onChange={this.handleChange} type="password" />
+                            placeholder="Password" onChange={this.handleChange}  type="password" />
                             <Form.Input fluid name="passwordConfirmation" icon="repeat" iconPosition="left"
-                            placeholder="Password Confirmation" onChange={this.handleChange} type="password" />
+                            placeholder="Password Confirmation" onChange={this.handleChange}  type="password" />
 
                             <Button color="orange" fluid size="large">Submit</Button>
                         </Segment>
@@ -30,7 +55,7 @@ class Register extends React.Component {
                     <Message>Already a user? <Link to="/login"></Link></Message>
                 </Grid.Column>
             </Grid>
-        )
+        );
     }
 }
 
