4de8e97e803014b40de08efaefcae60aa3e9092b
Acalltoredemption <acalltoredemption@gmail.com>
2021-08-27 17:12:27 -0700

    routing and setting up register component

---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase/app';
+import "firebase/auth";
+import "firebase/database";
+import "firebase/storage";
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBHQSR_C2hB4tJuMn_EPNiiulhAGyBKKwg",
+    authDomain: "react-slack-clone-24be5.firebaseapp.com",
+    projectId: "react-slack-clone-24be5",
+    storageBucket: "react-slack-clone-24be5.appspot.com",
+    messagingSenderId: "888181529765",
+    appId: "1:888181529765:web:fef5b6749761d30db9814c",
+    measurementId: "G-82LYT3MD76",
+    
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  export default firebase;
