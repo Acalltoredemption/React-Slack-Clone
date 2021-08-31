2d71d8efb35516264402ddcb03d20196171399d3
Acalltoredemption <acalltoredemption@gmail.com>
2021-08-31 07:21:44 -0700

    setting up our redux for handling user data

--- src/index.js
+++ src/index.js
@@ -3,18 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
-
+import rootReducer from './reducers';
 import {BrowserRouter as Router, Switch, Route, withRouter} from 'react-router-dom';
 import Login from './components/Auth/Login'
 import Register from './components/Auth/Register';
 import firebase from './firebase';
-
+import {createStore} from 'redux';
+import {Provider, connect} from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import 'semantic-ui-css/semantic.min.css';
+import { setUser } from './actions';
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 class Root extends React.Component {
     componentDidMount(){
         firebase.auth().onAuthStateChanged(user => {
             if(user){
+                this.props.setUser(user);
                 this.props.history.push('/');
             }
         })
@@ -32,10 +38,12 @@ class Root extends React.Component {
     }
 };
 
-const RootWithAuth = withRouter(Root);
+const RootWithAuth = withRouter(connect(null, {setUser})(Root));
 
 ReactDOM.render(
+    <Provider store={store}>
     <Router>
     <RootWithAuth />
-    </Router>, document.getElementById('root'));
+    </Router>
+    </Provider>, document.getElementById('root'));
 registerServiceWorker();
