5ad8c91c920049b087869d2107a025f1b8a7af4c
Acalltoredemption <acalltoredemption@gmail.com>
2021-08-30 17:39:16 -0700

    routing to bring logged in user to homepage

--- src/index.js
+++ src/index.js
@@ -4,21 +4,38 @@ import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route, withRouter} from 'react-router-dom';
 import Login from './components/Auth/Login'
 import Register from './components/Auth/Register';
+import firebase from './firebase';
 
 import 'semantic-ui-css/semantic.min.css';
 
-const Root = () => (
-    <Router>
+class Root extends React.Component {
+    componentDidMount(){
+        firebase.auth().onAuthStateChanged(user => {
+            if(user){
+                this.props.history.push('/');
+            }
+        })
+    }
+
+
+    render(){
+    return(
         <Switch>
             <Route exact path="/" component={App} />
             <Route exact path="/login" component={Login} />
             <Route exact path="/register" component={Register} />
         </Switch>
-    </Router>
-);
+    )
+    }
+};
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+const RootWithAuth = withRouter(Root);
+
+ReactDOM.render(
+    <Router>
+    <RootWithAuth />
+    </Router>, document.getElementById('root'));
 registerServiceWorker();
