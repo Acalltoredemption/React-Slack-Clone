f93ae0ec7aae60ce81c824304e867cc0b3e8306f
Acalltoredemption <acalltoredemption@gmail.com>
2021-08-31 19:04:15 -0700

    displaying via state displayname and avatar

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -4,6 +4,7 @@ import UserPanel from './UserPanel';
 
 class SidePanel extends React.Component {
     render(){
+        const {currentUser} = this.props;
         return(
             <Menu size="large"
             inverted
@@ -11,7 +12,7 @@ class SidePanel extends React.Component {
             vertical
             style={{background: '4c3c4c', fontsize: '1.2rem'}}>
 
-            <UserPanel />
+            <UserPanel currentUser={currentUser}/>
             </Menu>
         )
     }
--- src/components/SidePanel/UserPanel.js
+++ src/components/SidePanel/UserPanel.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import firebase from '../../firebase';
-import {Dropdown, Grid, Header, Icon} from 'semantic-ui-react';
+import {Dropdown, Grid, Header, Icon, Image} from 'semantic-ui-react';
 
 class UserPanel extends React.Component {
+    state = {
+        user: this.props.currentUser
+    }
+
+
+
+
     dropdownOptions = () => [
         {
             key: "user",
-            text: <span>Signed in as <strong>User</strong></span>,
+            text: <span>Signed in as <strong>{this.state.user.displayName}</strong></span>,
             disabled: true
         },
         {
@@ -26,6 +33,7 @@ class UserPanel extends React.Component {
         .then(() => console.log("signed out!"));
     }
     render(){
+        const {user} = this.state;
         return (
             <Grid style={{background: '#4c3c4c'}}>
                 <Grid.Column>
@@ -37,7 +45,10 @@ class UserPanel extends React.Component {
                     </Grid.Row>
                     <Header style={{padding: '0.25em'}} as="h4" inverted>
                         <Dropdown trigger={
-                            <span>User</span>
+                            <span>
+                                <Image src={user.photoURL} spaced="right" avatar />
+                                {user.displayName}
+                            </span>
                         } options={
                             this.dropdownOptions()
                         } />
@@ -48,5 +59,7 @@ class UserPanel extends React.Component {
         )
     }
 }
+
+
 
 export default UserPanel;
