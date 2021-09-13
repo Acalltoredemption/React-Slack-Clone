839dcc1dbe1efbaf951894b4837b3f74486fb58d
Acalltoredemption <acalltoredemption@gmail.com>
2021-09-13 16:31:43 -0700

    displaying top posters for each channel in metapanel

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ import SidePanel from './SidePanel/SidePanel';
 import Messages from './Messages/Messages';
 import MetaPanel from './MetaPanel/MetaPanel';
 
-const App = ({currentUser, currentChannel, isPrivateChannel}) => {
+const App = ({currentUser, currentChannel, isPrivateChannel, userPosts}) => {
   return(
   <Grid columns="equal" className="app" style={{background: '#eee'}}>
     <ColorPanel />
@@ -25,6 +25,7 @@ const App = ({currentUser, currentChannel, isPrivateChannel}) => {
 
     <Grid.Column width={4}>
     <MetaPanel
+    userPosts={userPosts}
     currentChannel={currentChannel}
     key={currentChannel && currentChannel.id}
     isPrivateChannel={isPrivateChannel} />
@@ -37,6 +38,7 @@ const mapStateToProps = state => ({
   currentUser: state.user.currentUser,
   currentChannel: state.channel.currentChannel,
   isPrivateChannel: state.channel.isPrivateChannel,
+  userPosts: state.channel.userPosts
 })
 
 export default connect(mapStateToProps)(App);
